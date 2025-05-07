38719f3aa3e1ccd1e211c719d93545e8f475b14f
ckhater <ckhater@1337>
2025-05-07 09:03:52 +0100

    ex03

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -2,6 +2,15 @@
 
 int main()
 {
-    std::string str="HI THIS IS BRAIN";
-    
+	std::string str="HI THIS IS BRAIN";
+	std::string* stringPTR = &str;
+	std::string& stringREF = str;
+
+	std::cout<<&str<<std::endl;
+	std::cout<<stringPTR<<std::endl;
+	std::cout<<&stringREF<<std::endl;
+	std::cout<<"******************"<<std::endl
+			 <<str<<std::endl
+			 <<*stringPTR<<std::endl
+			 <<stringREF<<std::endl;
 }
