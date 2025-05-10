9e33f4991d44e8bcfe701c2b9a5d9b347296c0ef
ckhater <ckhater@1337>
2025-05-10 21:27:27 +0100

    sf

--- ex04/main.cpp
+++ ex04/main.cpp
@@ -2,17 +2,42 @@
 #include<string>
 #include<fstream>
 
+void    fct_replace(std::string s1,std::string s2)
+{
+    return;
+}
+
 int main(int ac, char **av)
 {
-    std::fstream nfile("nfile");
-    std::fstream file("file");
+    std::fstream file;
+    std::fstream nfile;
 
-    // if(file.open("file.replace",std::fstream))
-
-    if(nfile.is_open())
-        std::cout<<"it is open nfile"<<std::endl;
+    if(ac == 4)
+    {
+        file.open("file.replace",std::ios::out | std::ios::trunc);
+        if(file.is_open())
+        {
+            nfile.open(*av+1,std::ios::in);
+            if(nfile.is_open())
+                fct_replace(*av+2,*av+3);
+            else{
+                std::cerr<<"\033[31mError\033[0m"<<std::endl;
+                file.close();
+                return(1);
+            }
+        }
+        else
+        {
+            std::cerr<<"\033[31mError\033[0m"<<std::endl;
+            return(1);
+        }
+    }
     else
-        std::cout<<"nooooooooooooooooooooot n fileee"<<std::endl;
+    {
+        std::cerr<<"\033[31mError\033[0m"<<std::endl
+                 <<"./replace <filename> string1 string2"<<std::endl;
+        return(1);
+    }
 
     return(0);
 }
